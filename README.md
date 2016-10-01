5ccc3165be5458eb8ff54038bf2402072cdd7453
sevensidedmarble <andrewianstewart@gmail.com>
2016-10-01 17:40:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# a simple starter project for love2d
+
+Based on some of the work by headchant at: https://github.com/headchant/love-boilerplate
---
+++ main.lua
@@ -0,0 +1,33 @@
+-- main.lua
+
+function love.load()
+	math.randomseed(os.time())
+	love.graphics.setDefaultFilter("nearest", "nearest")
+end
+
+function love.update( dt )
+	
+end
+
+function love.draw()
+
+end
+
+function love.keypressed()
+	
+end
+
+function love.keyreleased()
+	
+end
+
+function love.mousepressed()
+	
+end
+
+function love.mousereleased()
+	
+end
+
+-- Get console output working with sublime text
+io.stdout:setvbuf("no")
